6ba980b5a6b352c2a8d126d09d35ed2f46b4eeb8
Lovisa Hallgren <lovisahallgren1@gmail.com>
2019-04-15 20:57:03 +0200

    started with activities page

---
+++ nextjs/components/Activities/index.js
@@ -0,0 +1,167 @@
+import React, {Component} from 'react';
+import styled from 'styled-components';
+import { Fragment } from 'react';
+import Layout from '../Layout';
+import Link from 'next/link';
+import PropTypes from 'prop-types';
+import NavIndicator from '../NavIndicator';
+import H1 from '../H1'
+import H2 from '../H2'
+import P from '../P'
+import Card from '../Card'
+import QuoteSmall from '../QuoteSmall'
+import SmallP from '../SmallP'
+import Line from '../Line'
+import LetterBig from '../LetterBig'
+import ReadMoreCard from '../ReadMoreCard';
+import ReadMoreButton from '../ReadMoreButton';
+import ArrowUp from '../SVGs/ArrowUp';
+import Form from '../Form';
+import nookies from 'nookies';
+
+class Activities extends Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        isHighContrastMode: null
+      }
+    }
+
+    componentDidMount() {
+      this.setState({
+        isHighContrastMode: nookies.get(this.state.ctx).contrast === "true" ? "true" : "false"
+      })
+    }
+
+    render() {
+        const isHighContrastMode = this.state.isHighContrastMode === "true"
+        return(
+            <Layout
+                isHighContrastMode={isHighContrastMode}
+                handleContrastMode={this.handleContrastMode}
+            >
+                <NavIndicator
+                style={
+                    isHighContrastMode
+                    ? { background: 'var(--secondary-pink)' }
+                    : { background: 'var(--primary-pink)' }
+                }
+                >
+                <LetterBig>A</LetterBig>
+                <div style={{alignItems: "flex-end"}}>
+                    <SmallP>
+                    <Link href='/'>
+                        <a href="">Home</a>
+                    </Link> > {this.props.activities[0].type}
+                    </SmallP>
+                    <H1>
+                    {this.props.activities[0].type}
+                    </H1>
+                </div>
+                </NavIndicator>
+                {this.props.activities.map(item => {
+                return (
+                    <div key={item.id}>
+                    <Card
+                        style={
+                        isHighContrastMode
+                            ? { background: 'var(--secondary-pink)' }
+                            : { background: 'var(--primary-pink)' }
+                        }
+                    >
+                        <SmallP>{item.category}</SmallP>
+                        <Line />
+                        <H2>{item.title}</H2>
+                        <P>{item.excerpt}</P>
+                        <Line />
+                    </Card>
+                    {item.image !== null ||
+                    item.image_description !== null ||
+                    item.content !== null ||
+                    item.quote !== null ||
+                    item.quote_name !== null ? (
+                        <div>
+                        <ReadMoreButton
+                            style={
+                            isHighContrastMode
+                                ? {
+                                    background: 'var(--secondary-pink)',
+                                    borderTop: '2px solid var(--primary-pink)'
+                                }
+                                : {
+                                    background: 'var(--primary-pink)',
+                                    border: 'none'
+                                }
+                            }
+                            topColor={isHighContrastMode ? 'var(--primary-pink)' : "var(--secondary-pink)" }
+                            bottomColor={isHighContrastMode ? 'var(--primary-tint-pink)' : "var(--secondary-tint-pink)"}
+                            display={'none'}
+                            marginTop={isHighContrastMode ? '-2.1rem' : '-2rem'}
+                        >
+                            <ReadMoreCard
+                            style={
+                                isHighContrastMode
+                                ? {
+                                    background: 'var(--secondary-pink)',
+                                    borderTop: '2px solid var(--primary-pink)'
+                                    }
+                                : { background: 'var(--secondary-pink)' }
+                            }
+                            background="var(--primary-pink)"
+                            >
+                            {item.image === false ? null : 
+                            <div>
+                                <img src={item.image || undefined} alt={item.name} />
+                                <div style={{ display: 'flex' }}>
+                                    <ArrowUp />
+                                    <P
+                                    style={{
+                                        marginTop: '-0.7rem',
+                                        fontWeight: 'bold',
+                                        width: '100%'
+                                    }}
+                                    >
+                                    {item.image_description}
+                                    </P>
+                                </div>
+                            </div>} 
+                            
+                            {item.quote !== '' ? (
+                                <div>
+                                <Line />
+                                <QuoteSmall style={{ marginTop: '1rem' }}>
+                                    "{item.quote}"
+                                </QuoteSmall>
+                                </div>
+                            ) : null}
+                            <SmallP
+                                fontStyle="italic"
+                                textAlign="right"
+                                margin="1rem 0"
+                            >
+                                {item.quote_name}
+                            </SmallP>
+                            <Line />
+                            <P>{item.content}</P>
+                            </ReadMoreCard>
+                        </ReadMoreButton>
+                        </div>
+                    ) : null}
+                    </div>
+                );
+                })}
+                <Form
+                    color="var(--secondary-pink)"
+                    title="Skulle du vilja delta eller starta en aktivitet?"
+                    name="Namn"
+                    phone="Telefonnummer"
+                    email="E-mail"
+                    submit="Skicka"
+                    subject="Ärende"
+                />
+            </Layout>
+        )
+    }
+}
+
+export default Activities;
---
+++ nextjs/pages/activities.js
@@ -0,0 +1,39 @@
+import Layout from '../components/Layout'
+import React, { Component, Fragment } from 'react'
+import axios from 'axios'
+import Link from 'next/link'
+import H2 from '../components/H2'
+import P from '../components/P'
+import Card from '../components/Card'
+import QuoteSmall from '../components/QuoteSmall'
+import SmallP from '../components/SmallP'
+import Line from '../components/Line'
+import Activities from '../components/Activities'
+
+export default class extends Component {
+
+  // Resolve promise and set initial props.
+  static async getInitialProps () {
+
+    // Make request for posts.
+    // const posts = await axios.get(`http://${process.env.HOST}/wp-json/wp/v2/posts`)
+    // const projects = await axios.get(`http://${process.env.HOST}/wp-json/wp/v2/project`)
+    const activities = await axios.get(`http://${process.env.HOST}/wp-json/wp/v2/activities`)
+
+    // Return response to posts object in props.
+    return {
+      // posts: posts.data,
+      // projects: projects.data,
+      activities: activities.data
+    }
+  }
+
+  render() {
+    return (
+      <>
+        <Activities {...this.props}/>
+
+      </>
+    )
+  }
+}
