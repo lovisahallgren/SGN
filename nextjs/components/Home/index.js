87a68a9bf45cd12c679fe27558560493c790520a
Lovisa Hallgren <lovisahallgren1@gmail.com>
2019-04-15 20:58:47 +0200

    started with home page

--- nextjs/components/Home/index.js
+++ nextjs/components/Home/index.js
@@ -3,8 +3,12 @@ import styled from 'styled-components';
 import { Fragment } from 'react';
 import Layout from '../Layout';
 import H1 from '../H1';
-import Card from '../Card';
+import H2 from '../H2';
+import HomeCard from '../HomeCard';
 import Navbar from '../Navbar';
+import LetterBig from '../LetterBig';
+import SmallP from '../SmallP';
+import ArrowRight from '../SVGs/ArrowRight';
 import nookies from 'nookies';
 
 import Link from 'next/link';
@@ -25,17 +29,67 @@ class Home extends Component {
   }
 
   render() {
+    console.log(this.props)
     const isHighContrastMode = this.state.isHighContrastMode === "true"
 
     return(
       <Layout>
-        <Card style={isHighContrastMode ? {background: "var(--secondary-red)"} : {background: "var(--primary-red)"}} />
-        <Card style={isHighContrastMode ? {background: "var(--secondary-red)"} : {background: "var(--primary-red)"}} />
-        <Card style={isHighContrastMode ? {background: "var(--secondary-red)"} : {background: "var(--primary-red)"}} />
-        <Card style={isHighContrastMode ? {background: "var(--secondary-red)"} : {background: "var(--primary-red)"}} />
-        <Card style={isHighContrastMode ? {background: "var(--secondary-red)"} : {background: "var(--primary-red)"}} />
-        <Card style={isHighContrastMode ? {background: "var(--secondary-red)"} : {background: "var(--primary-red)"}} />
-        <Card style={isHighContrastMode ? {background: "var(--secondary-red)"} : {background: "var(--primary-red)"}} />
+        <HomeCard style={isHighContrastMode ? {background: "var(--secondary-purple)"} : {background: "var(--primary-purple)"}}>
+          <div style={{display: "flex", justifyContent: "space-between", height: "50%"}}>
+            <Link href="/projects">
+              <SmallP margin="0.3rem 0 0 0" textDecoration="underline">{this.props.projects[0].type}</SmallP>
+            </Link>
+            <LetterBig>P</LetterBig>
+          </div>
+          <div style={{display: "flex", justifyContent: "space-between", alignItems: "flex-end", height: "50%"}}>
+            <H2>{this.props.posts[0].title.rendered}</H2>
+            <Link href="/projects">
+              <ArrowRight />
+            </Link>
+          </div>
+        </HomeCard>
+        <HomeCard style={isHighContrastMode ? {background: "var(--secondary-purple)"} : {background: "var(--primary-purple)"}}>
+          <div style={{display: "flex", justifyContent: "space-between", height: "50%"}}>
+            <Link href="/projects">
+              <SmallP margin="0.3rem 0 0 0" textDecoration="underline">{this.props.projects[0].type}</SmallP>
+            </Link>
+            <LetterBig>P</LetterBig>
+          </div>
+          <div style={{display: "flex", justifyContent: "space-between", alignItems: "flex-end", height: "50%"}}>
+            <H2>{this.props.posts[0].title.rendered}</H2>
+            <Link href="/projects">
+              <ArrowRight />
+            </Link>
+          </div>
+        </HomeCard>
+        <HomeCard style={isHighContrastMode ? {background: "var(--secondary-pink)"} : {background: "var(--primary-pink)"}}>
+          <div style={{display: "flex", justifyContent: "space-between", height: "50%"}}>
+            <Link href="/activities">
+              <SmallP margin="0.3rem 0 0 0" textDecoration="underline">{this.props.activities[0].type}</SmallP>
+            </Link>
+            <LetterBig>A</LetterBig>
+          </div>
+          <div style={{display: "flex", justifyContent: "space-between", alignItems: "flex-end", height: "50%"}}>
+            <H2>{this.props.posts[0].title.rendered}</H2>
+            <Link href="/activities">
+              <ArrowRight />
+            </Link>
+          </div>
+        </HomeCard>
+        <HomeCard style={isHighContrastMode ? {background: "var(--secondary-pink)"} : {background: "var(--primary-pink)"}}>
+          <div style={{display: "flex", justifyContent: "space-between", height: "50%"}}>
+            <Link href="/activities">
+              <SmallP margin="0.3rem 0 0 0" textDecoration="underline">{this.props.activities[0].type}</SmallP>
+            </Link>
+            <LetterBig>A</LetterBig>
+          </div>
+          <div style={{display: "flex", justifyContent: "space-between", alignItems: "flex-end", height: "50%"}}>
+            <H2>{this.props.posts[0].title.rendered}</H2>
+            <Link href="/activities">
+              <ArrowRight />
+            </Link>
+          </div>
+        </HomeCard>
       </Layout>
 
     )
---
+++ nextjs/components/HomeCard/index.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+const StyledHomeCard = styled.div`
+  width: 100%;
+  height: 21rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: stretch;
+  background: ${props => props.background || 'white'};
+  color: ${props => props.color || 'white'};
+  padding: 16px 16px 20px 16px;
+  margin: 16px 0;
+
+  svg {
+    fill: #fff;
+    width: 25%;
+    height: 20%;
+    /* margin: 0.1rem 1rem; */
+  }
+`;
+
+class HomeCard extends Component {
+  constructor(props) {
+    super();
+    this.state = {
+
+    }
+  }
+
+  render() {
+    const { children } = this.props;
+    return (
+      <StyledHomeCard style={this.props.style}>
+        {children}
+      </StyledHomeCard>
+    );
+  }
+}
+
+HomeCard.propTypes = {
+
+};
+
+export default HomeCard;
