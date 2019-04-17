d8d94ef09e874ea0f724b0fb6b01242d49b97475
Lovisa Hallgren <lovisahallgren1@gmail.com>
2019-04-17 22:45:53 +0200

    changed font size to not grow when increasing font size on click

--- nextjs/components/H1/index.js
+++ nextjs/components/H1/index.js
@@ -1,16 +1,14 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import styled from 'styled-components';
 
 const H1 = styled.h1`
   font-weight: ${props => props.fontWeight || 'bold'};
-  line-height: ${props => props.lineHeight || '2.25rem'};
-  font-size: ${props => props.fontSize || '2rem'};
+  line-height: ${props => props.lineHeight || '36px'};
+  font-size: ${props => props.fontSize || '32px'};
   color: ${props => props.color || '#FFF'};
   text-transform: ${props => props.textTransform || 'capitalize'};
-`
+`;
 
-H1.propTypes = {
-
-};
+H1.propTypes = {};
 
 export default H1;
--- nextjs/components/LetterBig/index.js
+++ nextjs/components/LetterBig/index.js
@@ -1,15 +1,13 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import styled from 'styled-components';
 
 const LetterBig = styled.p`
   font-weight: ${props => props.fontWeight || 'bold'};
-  font-size: ${props => props.fontSize || '5.625rem'};
-  line-height: ${props => props.lineHeight || '4.5rem'};
-  color: ${props => props.color || '#FFF'}; 
-`
+  font-size: ${props => props.fontSize || '90px'};
+  line-height: ${props => props.lineHeight || '82px'};
+  color: ${props => props.color || '#FFF'};
+`;
 
-LetterBig.propTypes = {
-
-};
+LetterBig.propTypes = {};
 
 export default LetterBig;
