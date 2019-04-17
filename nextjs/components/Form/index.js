45aa68d718f7177fe164781d57cf825d341fd88c
Lovisa Hallgren <lovisahallgren1@gmail.com>
2019-04-17 22:46:53 +0200

    added id on form to make scroll work

--- nextjs/components/Form/index.js
+++ nextjs/components/Form/index.js
@@ -21,111 +21,105 @@ const StyledForm = styled.form`
     margin: 16px 0;
   }
 
-  .form-button-div{
-      padding-left: 70%;
-      padding-top: 5%;
+  .form-button-div {
+    padding-left: 70%;
+    padding-top: 5%;
+  }
+  .form-button {
+    text-decoration: underline;
+    font-weight: 800;
+    font-size: 1rem;
+    display: flex;
+    flex-direction: row;
+  }
+  .form-header {
+    padding-bottom: 10%;
+    padding-top: 10%;
+  }
 
-
-    }
-    .form-button{
-        text-decoration: underline;
-        font-weight:800;
-        font-size: 1rem;
-        display:flex;
-        flex-direction: row;
-      }
-      .form-header{
-        padding-bottom: 10%;
-        padding-top: 10%;
-      }
-
-.user-input-wrp {
-	position: relative;
-	width: 100%;
-  color: #333333;
-  padding-bottom: 10%;
-}
-.user-input-wrp .inputText{
-  font-size: 1.5rem;
-	width: 100%;
-	outline: none;
-	border:none;
-	border-bottom: 1px solid black;
- 	box-shadow: none !important;
-}
-.user-input-wrp .inputText:focus{
-	border-color: ${props => props.formColor || 'white'};
-	border-width: medium medium 1px;
-}
-.user-input-wrp .floating-label {
-	position: absolute;
-	pointer-events: none;
-	top: 18px;
-	left: 0%;
-	transition: 0.2s ease all;
-}
-.user-input-wrp input:focus ~ .floating-label,
-.user-input-wrp input:not(:focus):valid ~ .floating-label{
-	top: 0px;
-	left: 0%;
-  color: ${props => props.formColor || 'white'};
-	// font-size: 13px;
-  font-weight: bold;
-	opacity: 1;
-}
-
-`
+  .user-input-wrp {
+    position: relative;
+    width: 100%;
+    color: #333333;
+    padding-bottom: 10%;
+  }
+  .user-input-wrp .inputText {
+    font-size: 1.5rem;
+    width: 100%;
+    outline: none;
+    border: none;
+    border-bottom: 1px solid black;
+    box-shadow: none !important;
+  }
+  .user-input-wrp .inputText:focus {
+    border-color: ${props => props.formColor || 'white'};
+    border-width: medium medium 1px;
+  }
+  .user-input-wrp .floating-label {
+    position: absolute;
+    pointer-events: none;
+    top: 18px;
+    left: 0%;
+    transition: 0.2s ease all;
+  }
+  .user-input-wrp input:focus ~ .floating-label,
+  .user-input-wrp input:not(:focus):valid ~ .floating-label {
+    top: 0px;
+    left: 0%;
+    color: ${props => props.formColor || 'white'};
+    // font-size: 13px;
+    font-weight: bold;
+    opacity: 1;
+  }
+`;
 
 class Form extends React.Component {
   constructor(props) {
-  super(props);
-  this.state = {addClass: false}
+    super(props);
+    this.state = { addClass: false };
   }
 
-  render(){
-
+  render() {
     return (
-
-      <StyledForm {...this.props}>
-
+      <StyledForm {...this.props} id="form">
         <div className="form-container">
-
-
-        <H2 color="#000" className="form-header">{this.props.title}</H2>
+          <H2 color="#000" className="form-header">
+            {this.props.title}
+          </H2>
 
           <div className="user-input-wrp">
-            <br/>
-            <input type="text" className="inputText" required/>
+            <br />
+            <input type="text" className="inputText" required />
             <span className="floating-label">{this.props.name}</span>
           </div>
 
           <div className="user-input-wrp">
-            <br/>
-            <input type="text" className="inputText" required/>
+            <br />
+            <input type="text" className="inputText" required />
             <span className="floating-label">{this.props.phone}</span>
           </div>
 
           <div className="user-input-wrp">
-            <br/>
-            <input type="text" className="inputText" required/>
+            <br />
+            <input type="text" className="inputText" required />
             <span className="floating-label">{this.props.email}</span>
           </div>
 
           <div className="user-input-wrp">
-            <br/>
-            <input type="textarea" className="inputText" required/>
+            <br />
+            <input type="textarea" className="inputText" required />
             <span className="floating-label">{this.props.subject}</span>
           </div>
 
           <div className="form-button-div">
             <button className="form-button" type="submit">
               <p>{this.props.submit}</p>
-              <ArrowRight width="20%"></ArrowRight>
+              <ArrowRight width="20%" />
             </button>
           </div>
         </div>
       </StyledForm>
-    )
+    );
   }
 }
 export default Form;
