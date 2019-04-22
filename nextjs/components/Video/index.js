8c4f67ad6a9e87ae6b423608378586331426bf14
Lovisa Hallgren <lovisahallgren1@gmail.com>
2019-04-22 22:09:29 +0200

    changed videos and added activity gif

--- nextjs/components/Video/index.js
+++ nextjs/components/Video/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import styled from 'styled-components';
 
 const StyledVideo = styled.video`
@@ -7,12 +7,50 @@ const StyledVideo = styled.video`
   margin-bottom: -0.3rem;
 `;
 
-const Video = props => {
-  return (
-    <StyledVideo controls autoPlay muted loop>
-      <source src="/static/images/hero-film-instagram.mp4" type="video/mp4" />
-    </StyledVideo>
-  );
-};
+class Video extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      videoSrc: '/static/images/sgn_hero_desktop.mp4'
+    };
+    this.checkIfWindowDidResize = this.checkIfWindowDidResize.bind(this);
+  }
+
+  componentDidMount() {
+    if (window.innerWidth > 900) {
+      this.setState({
+        videoSrc: '/static/images/sgn_hero_desktop.mp4'
+      });
+    } else {
+      this.setState({
+        videoSrc: '/static/images/sgn_hero_mobile.mp4'
+      });
+    }
+
+    window.addEventListener('resize', () => {
+      this.checkIfWindowDidResize();
+    });
+  }
+
+  checkIfWindowDidResize = () => {
+    if (window.innerWidth > 900) {
+      this.setState({
+        videoSrc: '/static/images/sgn_hero_desktop.mp4'
+      });
+      return;
+    }
+    this.setState({
+      videoSrc: '/static/images/sgn_hero_mobile.mp4'
+    });
+  };
+
+  render() {
+    return (
+      <StyledVideo controls autoPlay muted loop>
+        <source src={this.state.videoSrc} type="video/mp4" />
+      </StyledVideo>
+    );
+  }
+}
 
 export default Video;
