349f6c60f03967d03c45d9d1238dd2bc8f273ed5
Lovisa Hallgren <lovisahallgren1@gmail.com>
2019-04-23 16:54:59 +0200

    added and populated social page

---
+++ nextjs/pages/social.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import SocialPage from '../components/SocialPage';
+
+export default class extends Component {
+  // Resolve promise and set initial props.
+  static async getInitialProps() {
+    // Make request for posts.
+    // const contact = await axios.get(
+    //   `http://${process.env.HOST}/wp-json/wp/v2/contact`
+    // );
+
+    // Return response to posts object in props.
+    return {
+      // contact: contact.data
+    };
+  }
+
+  render() {
+    return (
+      <>
+        <SocialPage {...this.props} />
+      </>
+    );
+  }
+}
